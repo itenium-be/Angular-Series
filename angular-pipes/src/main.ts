c068ce9bf920ae93b0a00ec87403ad14a07e0ef0
Wouter Van Schandevijl <woutervs@hotmail.com>
2024-06-01 16:04:16 +0200

    print angular version at startup^

--- angular-pipes/src/main.ts
+++ angular-pipes/src/main.ts
@@ -1,6 +1,9 @@
+import { VERSION } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+console.log('Angular', VERSION.full);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
