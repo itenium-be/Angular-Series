93061505ec70ba7fc89f31e71115ae031f9c7c62
Wouter Van Schandevijl <woutervs@hotmail.com>
2024-06-01 16:54:22 +0200

    setting default locale options

--- angular-pipes/src/app/app.config.ts
+++ angular-pipes/src/app/app.config.ts
@@ -1,5 +1,5 @@
-import { CurrencyPipe, JsonPipe, registerLocaleData } from '@angular/common';
-import { ApplicationConfig, LOCALE_ID } from '@angular/core';
+import { CurrencyPipe, DATE_PIPE_DEFAULT_OPTIONS, JsonPipe, registerLocaleData } from '@angular/common';
+import { ApplicationConfig, DEFAULT_CURRENCY_CODE, LOCALE_ID } from '@angular/core';
 
 
 // https://angular.dev/guide/i18n
@@ -15,6 +15,10 @@ registerLocaleData(localeFr, 'fr');
 export const appConfig: ApplicationConfig = {
   providers: [
     {provide: LOCALE_ID, useValue: 'fr'},
+
+    // Set global defaults for | currency and | date
+    // {provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR'},
+    // {provide: DATE_PIPE_DEFAULT_OPTIONS, useValue: {dateFormat: 'MM/dd/yy', timezone: '-1200'}},
 
     // Provide explicitly because we are injecting
     // these pipes in the app.component constructor
